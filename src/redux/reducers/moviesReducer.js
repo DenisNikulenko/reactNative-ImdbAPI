2f19419b64bc6d1337f3639e2af345d7a48d7e43
Denis <delttol@gmail.com>
2021-02-13 15:35:34 +0200

    added asyncStorage

--- src/redux/reducers/moviesReducer.js
+++ src/redux/reducers/moviesReducer.js
@@ -1,34 +1,56 @@
 import {
-    FETCHING_MOVIES_FAILURE,
-    FETCHING_MOVIES_REQUEST,
-    FETCHING_MOVIES_SUCCESS,
-  } from '../types';
+  FETCHING_MOVIES_FAILURE,
+  FETCHING_MOVIES_REQUEST,
+  FETCHING_MOVIES_SUCCESS,
+  ADD_TO_BOOKMARKS,
+  REMOVE_FROM_BOOKMARKS,
+} from '../types';
 
 const initialState = {
-    movies: [],
-    bookmarks: [],
-    isFetching: false,
-    errorMessage: "",
-}
+  movies: [],
+  bookmarksList: [],
+  isFetching: false,
+  errorMessage: '',
+};
 
 const moviesReducer = (state = initialState, action) => {
-    switch(action.type) {
-        case FETCHING_MOVIES_REQUEST:
-            return {
-                ...state, isFetching: true
-            }
-        case FETCHING_MOVIES_FAILURE:
-            return {
-                ...state, isFetching: false, errorMessage: action.payload
-            }
-        case FETCHING_MOVIES_SUCCESS: 
-            return {
-                ...state, isFetching: false, movies: action.payload
-            }
+  switch (action.type) {
+    case FETCHING_MOVIES_REQUEST:
+      return {
+        ...state,
+        isFetching: true,
+      };
 
-        default:
-            return state
-    }
-}
+    case FETCHING_MOVIES_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        movies: action.payload,
+      };
+
+    case ADD_TO_BOOKMARKS: 
+      return {
+          ...state,
+          bookmarksList:[...state.bookmarksList, action.payload]
+      }
+      
+      case REMOVE_FROM_BOOKMARKS: 
+      return {
+          ...state,
+          bookmarksList: state.bookmarksList.filter(movie => movies.id !== action.payload.id) 
+
+      }
+
+    case FETCHING_MOVIES_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        errorMessage: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
 
 export default moviesReducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,21 @@
 import {createStore, combineReducers, applyMiddleware} from "redux";
-import moviesReducer from "./reducers/moviesReducer"
 import thunk from "redux-thunk";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { persistStore, persistReducer } from 'redux-persist';
+
+import moviesReducer from "./reducers/moviesReducer";
+
+const persistConfig = {
+    key: "root",
+    storage: AsyncStorage 
+}
 
 const rootReducer = combineReducers({
-    movies: moviesReducer
+    movies: persistReducer(persistConfig, moviesReducer)
 })
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 const store = createStoreWithMiddleware(rootReducer)
+const appPersist = persistStore(store)
 
-export default store;
+export default {store, appPersist};
