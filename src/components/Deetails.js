89f54a6f51a289d919a8a5650549d77a56bd35ee
Denis <delttol@gmail.com>
2021-02-14 16:36:28 +0200

    updated styles for component details

--- src/components/Deetails.js
+++ src/components/Deetails.js
@@ -50,21 +50,34 @@ export const Details = ({movieDetails}) => {
   } = container;
 
   let title = movieDetails.title;
+  typeof title === 'string'
+    ? title.length > 30
+      ? (title = `${title.substr(0, 29)}`)
+      : null
+    : null;
 
-  typeof title === "string" ? title.length > 30 ? title = `${title.substr(0,29)}` : null : null
+  let genre;
+  genres
+    ? (genre = genres.map((g) => (
+        <View key={g.id}>
+          <Text style={{}}>{g.name}</Text>
+        </View>
+      )))
+    : (genre = <Text>Пусто</Text>);
 
   return (
     <ScrollView style={styles.container}>
-      <View >
-        <ImageBackground
-          resizeMethod="scale"
 
-          style={styles.image}
-          source={{
-            uri: `${IMAGE_URL}/${poster_path}`,
-          }}
-        />
-        <View style={styles.headerOpacitty}></View>
+      <View>
+          <ImageBackground
+            resizeMethod="scale"
+            style={styles.image}
+            source={{
+              uri: `${IMAGE_URL}/${poster_path}`,
+            }}
+          />
+
+          <View style={styles.opacityBlockTop}></View>
           <View style={styles.title}>
             <TouchableOpacity
               style={styles.buttonBack}
@@ -72,40 +85,69 @@ export const Details = ({movieDetails}) => {
               onPress={() => navigation.goBack()}>
               <Ionicons
                 onPress={() => navigation.goBack()}
-                style={{color: "white", width:50, height:50}}
+                style={{color: 'white', width: 50, height: 50}}
                 name="return-down-back"
                 size={40}
               />
             </TouchableOpacity>
-              <Text style={styles.textTitle}>{title}</Text>
+            <Text style={styles.textTitle}>{title}</Text>
+          </View>
+          
+
+          <View style={styles.opacityBlockBottom}></View>
+          <View style={styles.descriptionBlockBottom}>
+            <Text style={{color: 'white', fontSize: 16}}>
+              Рейтинг: <Text style={styles.textBold}>{vote_average}</Text>
+            </Text>
+            <Text style={{color: 'white'}}>
+              Длительность: <Text style={styles.textBold}>{runtime}</Text> мин
+            </Text>
+          </View>
+
+          <View style={styles.descriptionBlockOverview}>
+            <View style={styles.descriptionBlockOverviewText}>
+              <Text style={styles.textBold}>Описание</Text>
+              <Text>{overview}</Text>
             </View>
-      </View>
+            <View style={styles.descriptionBlockOverviewGenres}>
+              <Text style={styles.descriptionBlockOverviewRelease}>
+                <Text style={{...styles.textBold}}>Резил:</Text> {release_date}{' '}
+              </Text>
+              <Text style={{...styles.textBold, paddingBottom: 5}}>Жанры:</Text>
+              {genre}
+              <Text style={styles.descriptionBlockOverviewBudget}>
+                <Text style={{...styles.textBold}}>Бюджет:</Text>
+                {budget}$
+              </Text>
+            </View>
+          </View>
+        </View>
     </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "lightgrey",
+    backgroundColor: 'lightgrey',
     width: Dimensions.get('screen').width,
     height: Dimensions.get('screen').height,
   },
-  headerOpacitty: {
-    backgroundColor: "black",
+  opacityBlockTop: {
+    backgroundColor: 'black',
     position: 'absolute',
     width: Dimensions.get('screen').width,
     height: Dimensions.get('screen').height / 20,
-    opacity: .3,
+    opacity: 0.3,
   },
   title: {
-    position: "absolute",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
+    position: 'absolute',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     width: Dimensions.get('screen').width,
   },
   image: {
-    resizeMode: "cover",
+    resizeMode: 'cover',
     borderRadius: 5,
     width: Dimensions.get('screen').width,
     height: Dimensions.get('screen').height / 2,
@@ -115,11 +157,70 @@ const styles = StyleSheet.create({
     height: Dimensions.get('screen').height / 18,
     width: Dimensions.get('screen').width / 8,
     marginLeft: 5,
-
   },
   textTitle: {
     marginRight: 8,
-    color: "white",
+    color: 'white',
     fontSize: 24,
-  }
+  },
+
+  opacityBlockBottom: {
+    backgroundColor: 'black',
+    position: 'absolute',
+    width: Dimensions.get('screen').width,
+    height: Dimensions.get('screen').height / 20,
+    opacity: 0.3,
+    top: Dimensions.get('screen').width / 1.08,
+  },
+
+  descriptionBlockBottom: {
+    position: 'absolute',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    width: Dimensions.get('screen').width,
+    top: Dimensions.get('screen').width / 1.08 + 10,
+  },
+
+  test: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    width: Dimensions.get('screen').width,
+  },
+
+  descriptionRaite: {
+    color: 'white',
+  },
+  descriptionTime: {},
+
+  textBold: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+
+  descriptionBlockOverview: {
+    flexDirection: 'row',
+    margin: 10,
+  },
+  descriptionBlockOverviewText: {
+    flex: 3,
+  },
+  descriptionBlockOverviewGenres: {
+    alignItems: 'center',
+    borderLeftWidth: 1,
+    borderLeftColor: 'white',
+    flex: 1,
+  },
+  descriptionBlockOverviewRelease: {
+    borderBottomWidth: 1,
+    borderBottomColor: 'white',
+    padding: 15,
+  },
+  descriptionBlockOverviewBudget: {
+    alignItems: 'center',
+    borderTopColor: 'white',
+    borderTopWidth: 1,
+    paddingTop: 15,
+  },
 });
--- src/utilities/apiUrl.js
+++ src/utilities/apiUrl.js
@@ -3,9 +3,3 @@ export const API_KEY = "api_key=a0365c3dfe181648feb572b2dbf405c8"
 export const IMAGE_URL= "https://www.themoviedb.org/t/p/w600_and_h900_bestv2"
 export const LANGUAGE = "language=ru"
 export const TOP_POPULAR = "movie/top_rated" 
-//https://www.themoviedb.org/t/p/w600_and_h900_bestv2/5aXp2s4l6g5PcMMesIj63mx8hmJ.jpg
-
-//755812
-///collection/{collection_id}/images
-
-// https://api.themoviedb.org/3/movie/755812/images?api_key=a0365c3dfe181648feb572b2dbf405c8 
